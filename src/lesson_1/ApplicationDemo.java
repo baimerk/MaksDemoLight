bc2d67758dae138b10658355d1717eebd8670180
Maksim Kim <baimer1311@gmail.com>
2021-08-18 20:54:06 +0300

    This is my code number 2

---
+++ src/lesson_1/ApplicationDemo.java
@@ -0,0 +1,19 @@
+package lesson_1;
+
+public class ApplicationDemo {
+    public static void main(String[] args) {
+        int number = 8;
+        double numberD = 2.5;
+        long numberL = 9;
+        float numberF = 10;
+        //numberD = number;
+        number =(int)numberD;
+        System.out.println(number);
+        System.out.println(numberD);
+        char Letter = 'F';
+        System.out.println("Hello World Masksik");
+        System.out.println(Letter);
+        System.out.println(numberL);
+        System.out.println(numberF);
+    }
+}
